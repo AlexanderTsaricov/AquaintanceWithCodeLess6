1f748907756db56f528662b7b68ca86da69fdd3e
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-04-02 14:43:07 +0200

    add less6:homework1

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿/*
+    Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+*/
+
+char[,] charMatrix = new char[,] { { 'H', 'e', 'l', 'l', 'o' }, { 'w', 'o', 'r', 'l', 'd' } };
+
+string GetStringFromMatrix(char[,] matrix)
+{
+    string str = "";
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            str += matrix[i, j];
+        }
+        str += " ";
+    }
+    return str;
+}
+string str = GetStringFromMatrix(charMatrix);
+Console.WriteLine(str);
