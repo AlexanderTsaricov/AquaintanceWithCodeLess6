4e0294e3308eb2e7254917752414ffe8d2dcc7d9
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-04-02 15:52:09 +0200

    add less6:homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+    (не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+    Сформировать строку, в которой слова расположены в обратном порядке. 
+    В полученной строке слова должны быть также разделены пробелами.
+*/
+
+string str = "hello world my friend";
+str += " ";
+int countSpace = 0;
+foreach (char e in str)
+{
+    if (e == ' ')
+    {
+        countSpace++;
+    }
+}
+Console.WriteLine(countSpace);
+string[] strArray = new string[countSpace];
+string word = "";
+int count = 0;
+int i = strArray.Length - 1;
+foreach (char e in str)
+{
+    if (e == ' ')
+    {
+        strArray[i] = word;
+        word = "";
+        i--;
+        continue;
+    }
+    word += e;
+}
+string flipStr = "";
+foreach (string e in strArray)
+{
+    flipStr = flipStr + e + " ";
+}
+
+Console.WriteLine(flipStr);
+
+
